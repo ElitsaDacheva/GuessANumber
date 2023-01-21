13c3a0f0fb48703b8b1080ddc0cb40244c51776b
Elitsa Dacheva <122827000+ElitsaDacheva@users.noreply.github.com>
2023-01-21 15:51:35 +0200

    Add code for game.

---
+++ GuessANumber.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessANumber {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		Random randomNum = new Random();
+		int computerNum = randomNum.nextInt(100);
+
+		while (true) {
+			System.out.println("Guess a number (1-100): ");
+			String input = scanner.nextLine();
+			int playerNum;
+			boolean isValid = true;
+
+			for (int i = 0; i < input.length(); i++) {
+				if (input.charAt(i) < 48 || input.charAt(i) > 57) {
+					isValid = false;
+					break;
+				}
+			}
+			if (isValid) {
+				playerNum = Integer.parseInt(input);
+				if (playerNum == computerNum) {
+					System.out.println("Congratulations! You guess the number.");
+					break;
+				} else if (playerNum > computerNum) {
+					System.out.println("Your turn is higher.");
+				} else  {
+					System.out.println("Your turn is lower.");
+				}
+			}
+			else {
+				System.out.println("Invalid input. Try again with enter the number.");
+			}
+		}
+	}
+}
---
+++ out/production/GuessANumber/README.md
@@ -0,0 +1,2 @@
+# GuessANumber
+This is a simple game to guess a number.
