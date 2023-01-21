ffae9ebb983bb86ae65fb9e352fe317c01d5e4c5
Elitsa Dacheva <122827000+ElitsaDacheva@users.noreply.github.com>
2023-01-21 16:46:54 +0200

    I add count for tries to get the number.

--- GuessANumber.java
+++ GuessANumber.java
@@ -7,13 +7,14 @@ public class GuessANumber {
 
 		Random randomNum = new Random();
 		int computerNum = randomNum.nextInt(100);
+		int countTry = 0;
 
 		while (true) {
 			System.out.println("Guess a number (1-100): ");
 			String input = scanner.nextLine();
 			int playerNum;
-			boolean isValid = true;
 
+			boolean isValid = true;
 			for (int i = 0; i < input.length(); i++) {
 				if (input.charAt(i) < 48 || input.charAt(i) > 57) {
 					isValid = false;
@@ -22,8 +23,10 @@ public class GuessANumber {
 			}
 			if (isValid) {
 				playerNum = Integer.parseInt(input);
+				countTry++;
 				if (playerNum == computerNum) {
 					System.out.println("Congratulations! You guess the number.");
+					System.out.println("Your tries to guess is " + countTry);
 					break;
 				} else if (playerNum > computerNum) {
 					System.out.println("Your turn is higher.");
